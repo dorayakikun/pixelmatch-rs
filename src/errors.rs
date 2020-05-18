dbdf4a2762bf237961882329cbe04f6defb6eb81
dorayakikun <github@dorayakikun.com>
2020-05-18 22:32:24 +0900

    Migrate from the `failure` to `anyhow`

--- src/errors.rs
+++ src/errors.rs
@@ -1,83 +1,37 @@
-use failure::{Backtrace, Context, Fail};
+use thiserror::Error;
 use image::ImageError;
-use std::fmt;
-use std::fmt::Display;
 use std::io::Error as IOError;
 use std::num::ParseFloatError;
 
-#[derive(Debug)]
-pub struct PixelMatchError {
-    inner: Context<ErrorKind>,
-}
-
-#[derive(Copy, Clone, Eq, PartialEq, Debug, Fail)]
-pub enum ErrorKind {
-    #[fail(display = "Is not number")]
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("Is not number")]
     ParseFloatError,
-    #[fail(display = "Failed to open image file")]
-    InvalidImageFile,
-    #[fail(display = "Image sizes does not match")]
-    SizeUnmatch,
-    #[fail(display = "Failed to save diff image")]
+    #[error("Failed to open image file. caused by {0}")]
+    InvalidImageFile(String),
+    #[error("Image sizes does not match. before: {before:?} after: {after:?}")]
+    SizeUnmatch {
+        before: (u32, u32),
+        after: (u32, u32),
+    },
+    #[error("Failed to save diff image")]
     IOError,
 }
 
-impl Fail for PixelMatchError {
-    fn cause(&self) -> Option<&dyn Fail> {
-        self.inner.cause()
-    }
-
-    fn backtrace(&self) -> Option<&Backtrace> {
-        self.inner.backtrace()
+impl From<ParseFloatError> for Error {
+    fn from(_error: ParseFloatError) -> Self {
+        Error::ParseFloatError
     }
 }
 
-impl Display for PixelMatchError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        Display::fmt(&self.inner, f)
+impl From<ImageError> for Error {
+    fn from(error: ImageError) -> Self {
+        Error::InvalidImageFile(error.to_string())
     }
 }
 
-impl PixelMatchError {
-    pub fn kind(&self) -> ErrorKind {
-        *self.inner.get_context()
-    }
-}
-
-impl From<ErrorKind> for PixelMatchError {
-    fn from(kind: ErrorKind) -> PixelMatchError {
-        PixelMatchError {
-            inner: Context::new(kind),
-        }
-    }
-}
-
-impl From<Context<ErrorKind>> for PixelMatchError {
-    fn from(inner: Context<ErrorKind>) -> PixelMatchError {
-        PixelMatchError { inner }
-    }
-}
-
-impl From<ParseFloatError> for PixelMatchError {
-    fn from(error: ParseFloatError) -> PixelMatchError {
-        PixelMatchError {
-            inner: error.context(ErrorKind::ParseFloatError),
-        }
-    }
-}
-
-impl From<ImageError> for PixelMatchError {
-    fn from(error: ImageError) -> PixelMatchError {
-        PixelMatchError {
-            inner: error.context(ErrorKind::InvalidImageFile),
-        }
-    }
-}
-
-impl From<IOError> for PixelMatchError {
-    fn from(error: IOError) -> PixelMatchError {
-        PixelMatchError {
-            inner: error.context(ErrorKind::IOError),
-        }
+impl From<IOError> for Error {
+    fn from(_error: IOError) -> Self {
+        Error::IOError
     }
 }
